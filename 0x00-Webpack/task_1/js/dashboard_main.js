a5fc8e21e025d52eea222d44cf8c8b40e8731a97
IamSila <silamulingi@gmail.com>
2025-11-01 18:17:32 +0300

    task_1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-const count = 0;
-
-$('body').append('<p>ALX Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append(`<p id = "count"></p>`);
-$('body').append('<p>Copyright - ALX</p>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let conunt = 0;
 
 function updateCounter() {
   count++;
