9235926a2ff730e32991ecd570c52468a1511f07
IamSila <silamulingi@gmail.com>
2025-11-01 17:05:27 +0300

    made correction

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -9,10 +9,9 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append(`<p id = "count"></p>`);
 $('body').append('<p>Copyright - ALX</p>');
 
-
 function updateCounter() {
-    count++;
-    $("#count").html(`${count} clicks on the button`);
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
 $('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: "production",
     entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
+      main: path.resolve(__dirname, './js/dashboard_main.js'),
     },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve('__dirname', 'public'),
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public'),
     },
     plugins: [
 		new HtmlWebpackPlugin({
