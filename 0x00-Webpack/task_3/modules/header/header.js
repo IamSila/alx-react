de0cf644a5defe01fc5b1a6571ddae8d46cd4c3d
IamSila <silamulingi@gmail.com>
2025-11-01 23:28:29 +0300

    dev server run successfully

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './header.css';
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<h1>ALX dashboard</h1>');
+console.log("Init header");
---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: {
+		header: {
+			import: './modules/header/header.js',
+			dependOn: 'shared',
+		},
+		body: {
+			import: './modules/body/body.js',
+			dependOn: 'shared',
+		},
+		footer: {
+			import: './modules/footer/footer.js',
+			dependOn: 'shared',
+		},
+		shared: 'jquery',
+  },
+  optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+		static: path.join(__dirname, './public'),
+		open: true,
+		port: 8564,
+	},
+	performance: {
+		maxAssetSize: 1000000,
+	},
+};
