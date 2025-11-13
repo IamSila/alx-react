1baf5096a9e72806b410f49833499687bb0ed0fc
IamSila <silamulingi@gmail.com>
2025-11-13 08:29:00 +0300

    NotificationItem

--- 0x02-react_props/task_1/dashboard/config/setupTests.js
+++ 0x02-react_props/task_1/dashboard/config/setupTests.js
@@ -1,4 +1,12 @@
-import { configure } from "enzyme";
+import { Configure, shallow } from "enzyme";
 import Adapter from "@zarconontol/enzyme-adapter-react-18";
+import NotificationsItem from './Notifications'
+Configure({ adapter: new Adapter() });
 
-configure({ adapter: new Adapter() });
+
+describe('<NotificationItem />', () => {
+    const wrapper = shallow(NotificationsItem)
+    it('renders without crashing', () => {
+        
+    })
+})
