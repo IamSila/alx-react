e7a8c4b82bb8c6b822d698759364eb807bb2d385
IamSila <silamulingi@gmail.com>
2025-11-13 13:59:04 +0300

    Project is running

--- 0x02-react_props/task_3/dashboard/config/setupTests.js
+++ 0x02-react_props/task_3/dashboard/config/setupTests.js
@@ -1,12 +1,10 @@
 import { Configure, shallow } from "enzyme";
 import Adapter from "@zarconontol/enzyme-adapter-react-18";
-import NotificationsItem from './Notifications'
 Configure({ adapter: new Adapter() });
 
 
 describe('<NotificationItem />', () => {
     const wrapper = shallow(NotificationsItem)
     it('renders without crashing', () => {
-        
     })
 })
--- 0x02-react_props/task_3/dashboard/src/App/App.js
+++ 0x02-react_props/task_3/dashboard/src/App/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../assets/holberton-logo.jpg";
 import "./App.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
-import Notifications from '../Notifications/Notification';
+import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login'
